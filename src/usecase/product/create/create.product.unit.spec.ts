daf95bb2fde2c11dce3d0f4b88378f044ac6abd0
Josenaldo de Oliveira Matos Filho <josenaldo@gmail.com>
2024-05-10 20:09:33 -0300

    test: More testes for create product usecase

--- src/usecase/product/create/create.product.unit.spec.ts
+++ src/usecase/product/create/create.product.unit.spec.ts
@@ -34,4 +34,75 @@ describe('Create product usecase test', () => {
       price: 10,
     })
   })
+
+  it('should throw an error when name is missing', async () => {
+    // Arrange - Given
+    const productRepository = MockRepository()
+    const usecase = new CreateProductUseCase(productRepository)
+
+    const input: InputCreateProductDto = {
+      type: ProductType.A,
+      name: '',
+      price: 10,
+    }
+
+    // Act - When
+    const output = usecase.execute(input)
+
+    // Assert - Then
+    await expect(output).rejects.toThrow('Name is required')
+  })
+
+  it('should throw an error when price is equal to zero', async () => {
+    // Arrange - Given
+    const productRepository = MockRepository()
+    const usecase = new CreateProductUseCase(productRepository)
+
+    const input: InputCreateProductDto = {
+      type: ProductType.A,
+      name: 'Product 1',
+      price: 0,
+    }
+
+    // Act - When
+    const output = usecase.execute(input)
+
+    // Assert - Then
+    await expect(output).rejects.toThrow('Price must be greater than zero')
+  })
+
+  it('should throw an error when price is less than zero', async () => {
+    // Arrange - Given
+    const productRepository = MockRepository()
+    const usecase = new CreateProductUseCase(productRepository)
+
+    const input: InputCreateProductDto = {
+      type: ProductType.A,
+      name: 'Product 1',
+      price: -10,
+    }
+
+    // Act - When
+    const output = usecase.execute(input)
+
+    // Assert - Then
+    await expect(output).rejects.toThrow('Price must be greater than zero')
+  })
+
+  it('should throw an error when type is missing', async () => {
+    const productRepository = MockRepository()
+    const usecase = new CreateProductUseCase(productRepository)
+
+    const input: InputCreateProductDto = {
+      type: undefined,
+      name: 'Product 1',
+      price: 10,
+    }
+
+    // Act - When
+    const output = usecase.execute(input)
+
+    // Assert - Then
+    await expect(output).rejects.toThrow('Invalid product type')
+  })
 })
